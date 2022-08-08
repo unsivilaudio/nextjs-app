9830d890c150f5b91cdab5d4c2ccb67ae2bf22c8
unsivil <unsivilaudio@gmail.com>
2022-08-08 15:36:50 -0600

    some other change

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function MainNavigation() {
     return (
         <header className={classes.header}>
-            <div className={classes.logo}>NextJS Meetups</div>
+            <div className={classes.logo}>NextJS Meetups 2</div>
             <nav>
                 <ul>
                     <li>
